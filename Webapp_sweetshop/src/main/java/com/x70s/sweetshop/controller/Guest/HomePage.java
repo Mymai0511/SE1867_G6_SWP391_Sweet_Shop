cbfb2f35cb38c4d8892f9769d5b609845778cc22
Tran_Cao_Hung <hungtche176766@fpt.edu.vn>
2024-09-14 17:32:42 +0700

    fornt cake main

--- Webapp_sweetshop/src/main/java/com/x70s/sweetshop/controller/Guest/HomePage.java
+++ Webapp_sweetshop/src/main/java/com/x70s/sweetshop/controller/Guest/HomePage.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class HomePage extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("cakeMain/home.jsp").forward(request,response);
+        request.getRequestDispatcher("page/cakeMain/Index.jsp").forward(request,response);
     }
 }
