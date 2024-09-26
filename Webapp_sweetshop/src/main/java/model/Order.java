36e1fbf7409b1d8efd2e887d0368a671901dcd0f
Tran_Cao_Hung <hungtche176766@fpt.edu.vn>
2024-09-26 17:10:52 +0700

    update checkout

---
+++ Webapp_sweetshop/src/main/java/model/Order.java
@@ -0,0 +1,23 @@
+package model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Order {
+    private int id;
+    private String phone;
+    private String email;
+    private String shipAddress;
+    private Date orderTime;
+    private Date deliveryTime;
+    private Date receiptTime;
+    private int status;
+    private String voucherCode;
+    private int userId;
+}
