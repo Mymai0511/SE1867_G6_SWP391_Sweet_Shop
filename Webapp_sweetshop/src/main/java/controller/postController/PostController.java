b7bb1937e0e574d2f7efd963edf41c8b1737644a
mduc1011-swp <duchmhe187354@fpt.edu.vn>
2024-10-01 23:07:37 +0700

    control

--- Webapp_sweetshop/src/main/java/controller/postController/PostController.java
+++ Webapp_sweetshop/src/main/java/controller/postController/PostController.java
@@ -2,5 +2,39 @@ package controller.postController;
 
 import jakarta.servlet.http.HttpServlet;
 
-public class PostController extends HttpServlet {
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    // Create Post
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        Post createdPost = postService.createPost(post);
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
+
+    // Update Post
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post post) {
+        Post updatedPost = postService.updatePost(id, post);
+        return ResponseEntity.ok(updatedPost);
+    }
+
+    // View Post
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPost(@PathVariable Long id) {
+        Post post = postService.getPostById(id);
+        return ResponseEntity.ok(post);
+    }
+
+    // List Posts
+    @GetMapping
+    public ResponseEntity<List<Post>> listPosts() {
+        List<Post> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
+    }
 }
+
